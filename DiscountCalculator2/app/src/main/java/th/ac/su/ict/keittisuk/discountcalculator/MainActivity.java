34972fd8312eaff74df702888f075d92f0e36fdd
Phanuphong_199 <45302598+morphiz199@users.noreply.github.com>
2019-03-31 21:42:01 +0700

    test

--- DiscountCalculator2/app/src/main/java/th/ac/su/ict/keittisuk/discountcalculator/MainActivity.java
+++ DiscountCalculator2/app/src/main/java/th/ac/su/ict/keittisuk/discountcalculator/MainActivity.java
@@ -2,13 +2,19 @@ package th.ac.su.ict.keittisuk.discountcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private SeekBar seekBarPercentOff;
-    private TextView percentOffText;
+    SeekBar seekBarPercentOff;
+    TextView percentOffText;
+
+    EditText editText;
+    CheckBox checkTax;
+    TextView showResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +24,10 @@ public class MainActivity extends AppCompatActivity {
         seekBarPercentOff = findViewById(R.id.seekBarPercentOff);
         percentOffText = findViewById(R.id.percentOffText);
 
+        editText = findViewById(R.id.editText);
+        checkTax = findViewById(R.id.checkTax);
+        showResult = findViewById(R.id.showResult);
+
 
         seekBarPercentOff.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
 
@@ -25,8 +35,37 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                progress_value = progress;
-                percentOffText.setText(progress+"%");
+                percentOffText.setText(seekBarPercentOff.getProgress()+"%");
+                if(editText.getText().length() != 0){
+                    float result1 = 0;
+                    result1 = Float.parseFloat(editText.getText().toString());
+
+                    float result2 = seekBarPercentOff.getProgress();
+
+                    float finalResult;
+
+                    float result3 = 0;
+                    float result4 = 0;
+                    float result5 = seekBarPercentOff.getProgress();
+
+                    result3 = (result2 / 100) * result1;
+
+                    float tax;
+
+                    if(checkTax.isChecked()) {
+                        finalResult = result1 - result3;
+                        tax = (float)0.07*finalResult;
+
+                        finalResult += tax;
+                        result4 = result3+finalResult;
+                    }
+
+                    else {
+                        finalResult = result1-result3;
+                    }
+
+                    showResult.setText(Float.toString(finalResult));
+                }
             }
 
             @Override
@@ -36,7 +75,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-                percentOffText.setText(progress_value);
+
 
             }
         });
